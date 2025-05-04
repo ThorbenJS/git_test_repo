0172c78f829eda6f645a9a846c847cf0345e2227
Benjamin_Sundell <benjamin.sundell@edu.turkuamk.fi>
2025-05-04 15:10:47 +0300

    Made changes to source_code.py

--- source_code.py
+++ source_code.py
@@ -1,3 +1,4 @@
 print("I am somewhat familiar with Git.")
 print("I have changed the code in this file")
 print("I have now solved the merge conflict")
+print("I have now made changes that I will push to a remote repo on GitHub")
