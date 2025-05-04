8fbe41cdb70577d582de85c2554eaf8fd987e066
Benjamin_Sundell <benjamin.sundell@edu.turukamk.fi>
2025-05-04 13:31:08 +0300

    changed the source_code.py file

--- source_code.py
+++ source_code.py
@@ -1,3 +1,4 @@
 print("I am somewhat familiar with Git.")
 print("I have changed the code in this file")
-print("I have now changed the code in this file in the 'development' branch")
+print("I have yet again changed the code in this file")
+print("This is a new line of code at the end for merging without conflict")
